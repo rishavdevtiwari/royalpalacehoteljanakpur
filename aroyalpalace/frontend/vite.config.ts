a48464c0190633e217d5f6b450fbcf05a2db3df6
rishavdevtiwari <rishavtiwari.ai@gmail.com>
2025-03-28 15:41:56 +0545

    update again

--- aroyalpalace/frontend/vite.config.ts
+++ aroyalpalace/frontend/vite.config.ts
@@ -1,17 +1,13 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   base: '/',
-  plugins: [
-    react(),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
