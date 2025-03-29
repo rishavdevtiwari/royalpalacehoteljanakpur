0417501ac2789bf7ae11c3376f7d0bc667fe9b98
rishavdevtiwari <rishavtiwari.ai@gmail.com>
2025-03-29 15:45:26 +0545

    changes made for frontend 404 error resolving

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 export default defineConfig({
-  base: '/royalpalacehotel/', // Base path for GitHub Pages
+  base: '/', // Base path for custom domain deployment
   plugins: [react()],
   resolve: {
     alias: {
